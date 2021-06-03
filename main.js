c6ff40cda56c1a3ddad1366bee579b1558a38d46
DeployChef <dc.titovvlad@gmail.com>
2021-06-03 10:38:42 +0300

    Task 0

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('123');
+const player1 = {
+    name: 'Scorpion',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: ['Harpoon'],
+    attack: function() {
+        console.log(this.name + ' Fight!');
+    }
+};
+
+const player2 = {
+    name: 'SubZero',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    weapon: ['Katana'],
+    attack: function() {
+        console.log(this.name + ' Fight!');
+    }
+};
+
+
+player1.attack();
+player2.attack();
