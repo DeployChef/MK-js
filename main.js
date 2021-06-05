87bd4f9bec7ff114c76059b6c62c30f499608cd3
DeployChef <dc.titovvlad@gmail.com>
2021-06-06 00:19:41 +0300

    Task 0

--- main.js
+++ main.js
@@ -62,6 +62,8 @@ function createPlayer(playerObj) {
 function changeHp(player) {
     const $playerLife = document.querySelector('.player' + player.player + ' .life');
     player.hp -= 20;
+    if (player.hp < 0)
+        player.hp = 0;
     $playerLife.style.width = player.hp + "%";
 
     if (player.hp < 0) {
